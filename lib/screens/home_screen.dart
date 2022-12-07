8eda22ab7e26c9955013c042b535291eb9554d1a
kim-thuita <kevinkoech265@gmail.com>
2022-12-07 18:53:11 +0300

    "FINALLY  Able to implement user sign and being able to log in !!wow very nice very nice "

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,29 +1,15 @@
+import 'package:basic_sign_implementation/screens/sign_in.dart';
 import 'package:basic_sign_implementation/screens/sign_up.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class Home_Screen extends StatefulWidget {
-  final Function(User) OnSignIn;
-
-  const Home_Screen({super.key, required this.OnSignIn});
-
   @override
   State<Home_Screen> createState() => _Home_ScreenState();
 }
 
 class _Home_ScreenState extends State<Home_Screen> {
-  User? user;
-  Future<void> _SignInAnonimously() async {
-    try {
-      final usercredentials = await FirebaseAuth.instance.signInAnonymously();
-      widget.OnSignIn(usercredentials.user!);
-      print("UserId:${usercredentials.user?.uid}");
-    } catch (e) {
-      print(e.toString());
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,67 +22,68 @@ class _Home_ScreenState extends State<Home_Screen> {
           backgroundColor: Colors.transparent,
           title: Text(
             "Simply_Living",
-            style: GoogleFonts.akshar(fontSize: 20, color: Colors.red),
+            style: GoogleFonts.akshar(fontSize: 20, color: Colors.amber),
           ),
         ),
         body: Column(
           children: [
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 22),
-              child: Image.asset("images/time4_image.jpg"),
-            ),
-            SizedBox(height: 15),
-            Text(
-              "Be A Better Version Of Yourself",
-              style: TextStyle(
-                color: Colors.amberAccent,
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 40),
-            GestureDetector(
-              onTap: () => Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Sign_Up())),
-              child: Container(
-                alignment: Alignment.center,
-                height: 55,
-                width: 350,
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  border: Border.all(
-                    color: Colors.amberAccent,
-                  ),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Text(
-                  "Create Account",
-                  style: GoogleFonts.aldrich(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 60),
             Container(
-              height: 55,
-              width: 350,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.orange,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    )),
-                onPressed: _SignInAnonimously,
-                child: Text(
-                  "Sign IN Anonymously",
-                  style: GoogleFonts.aldrich(
-                    color: Colors.white,
-                    fontSize: 15,
-                  ),
-                ),
+              alignment: Alignment.center,
+              padding: EdgeInsets.symmetric(vertical: 150, horizontal: 20),
+              child: Icon(
+                FontAwesomeIcons.airbnb,
+                size: 100,
               ),
             ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                InkWell(
+                  onTap: () => Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Sign_Up())),
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 325,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.deepPurple, Colors.red],
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      "Create Account",
+                      style: GoogleFonts.openSans(
+                          fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("Already Have An Account ?"),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    GestureDetector(
+                        onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Sign_In(),
+                              ),
+                            ),
+                        child: Text(
+                          "Log In",
+                          style: GoogleFonts.openSans(
+                              color: Colors.grey, fontSize: 20),
+                        ))
+                  ],
+                )
+              ],
+            )
           ],
         ),
       ),
